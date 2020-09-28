41caf8d460b55b8ba5bf201c05e18b31de553d25
sebastian velasquez <sebasm4a1@hotmail.com>
2020-09-28 01:58:32 -0500

    modifique el current

--- scipt/main.js
+++ scipt/main.js
@@ -3,10 +3,7 @@ const carrouselStripe = document.querySelector('.image');
 let current = 0;
 
 function handleNextSlide () {
-  current++;
-  if(current >= carrouselStripe.children.length) {
-    current = 0;
-  }
+ 
   const width = carrousel.clientWidth;
-  carrouselStripe.style.transform = 'translate(-' + (width * current) + 'px, 0px)';
+  carrouselStripe.style.transform = 'translate(-' + (width ) + 'px, 0px)';
 }
