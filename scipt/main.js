212668c88185d02cc08d29733b09bd38968f981d
sebastian velasquez <sebasm4a1@hotmail.com>
2020-09-28 02:00:57 -0500

    añadi las imagenes de los productos y sigo con la galeria

--- scipt/main.js
+++ scipt/main.js
@@ -3,7 +3,10 @@ const carrouselStripe = document.querySelector('.image');
 let current = 0;
 
 function handleNextSlide () {
- 
+  current++;
+  if(current >= carrouselStripe.children.length) {
+    current = 0;
+  }
   const width = carrousel.clientWidth;
-  carrouselStripe.style.transform = 'translate(-' + (width ) + 'px, 0px)';
+  carrouselStripe.style.transform = 'translate(-' + (width * current) + 'px, 0px)';
 }
