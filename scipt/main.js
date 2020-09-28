165cfdbc99a8a0142c852eb1d4394fe60732e76e
sebastian velasquez <sebasm4a1@hotmail.com>
2020-09-28 02:36:24 -0500

    termine la galeria

--- scipt/main.js
+++ scipt/main.js
@@ -16,12 +16,12 @@ function handleNextSlide () {
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
